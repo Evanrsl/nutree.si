b47222d07624d1a06cd2aaa4a3f5edfb9bfaf381
Evan Christopher <evanchristopher1907@gmail.com>
2022-08-26 20:04:29 +0200

    test

--- src/screens/List.tsx
+++ src/screens/List.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import AppBar from '../components/app-bar'
 import FoodGroup from '../components/FoodGroup'
 import FoodItem from '../components/FoodItem'
-import * as foods from '../data/foods.json'
+import foods from '../data/foods.json'
 
 
 export default function List() {
@@ -15,18 +15,15 @@ export default function List() {
     >
       <AppBar title="Food Library" />
       <ScrollView flex={1} w={'100%'} bg={'muted.900'} pt={6}>
-        {/* { 
-          Object.entries(foods).forEach(([group, foodItems]) => {
-            if (foodItems.length && group) {
+        <VStack>
+          { 
+            Object.entries(foods).forEach(([group, foodItems]) => {
+              console.log(foodItems.slice(0,1))
               return <FoodGroup name={group} foods={foodItems} />
-            }
+            })
           }
-          )
-        } */}
-        <FoodGroup
-          name='Cereals'
-          foods={[ {Code: '1', Name: "Satay Ayam", Type: "bumbu kacang", Calories: "1000"} ]}
-        />
+        </VStack>
+        
         <FoodGroup
           name='Cereals'
           foods={[ {Code: '1', Name: "Satay Ayam", Type: "bumbu kacang", Calories: "1000"} ]}
