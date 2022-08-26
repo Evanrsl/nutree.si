e7b5d8ca491fcd8c105783b5f120409e7cc706fc
Evan Russel <66687431+evanrussel15@users.noreply.github.com>
2022-08-26 19:21:12 +0700

    bug fix app-bar

--- src/components/app-bar.tsx
+++ src/components/app-bar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { VStack, HStack, Button, IconButton, Icon, Text, NativeBaseProvider, Center, Box, StatusBar } from 'native-base';
 import { MaterialIcons } from '@expo/vector-icons';
 
-export default function AppBar({title}) {
+export default function AppBar({ title="" }) {
     return <>
         <Box safeAreaTop bg="muted.800" />
         <HStack bg="muted.800" px="5" py="3" justifyContent="space-between" alignItems="center" w="100%">
